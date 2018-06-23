8e60d82904067044d2ac16446796afc5383c8776
Konstantin Korostelev <koriman2016@gmail.com>
2018-06-24 00:20:15 +0400

    lesson02

---
+++ src/lesson02/QuickSort.java
@@ -0,0 +1,44 @@
+package lesson02;
+
+/**
+ * Sort an array by Quick sort algorithm
+ *
+ */
+public class QuickSort {
+
+    public static void sort(int[] array) {
+        sortHelper(array, 0, array.length - 1);
+    }
+
+    public static void sortHelper(int[] array, int left, int right) {
+        if (right > left) {
+            int index = partition(array, left, right);
+            sortHelper(array, left, index - 1);
+            sortHelper(array, index, right);
+        }
+    }
+
+    public static int partition(int[] array, int left, int right) {
+        int pivot = array[(left + right) / 2];
+
+        while (left <= right) {
+            while (array[left] < pivot) {
+                ++left;
+            }
+
+            while (array[right] > pivot) {
+                --right;
+            }
+
+            if (left <= right) {
+                int tmp = array[left];
+                array[left] = array[right];
+                array[right] = tmp;
+                ++left;
+                --right;
+            }
+        }
+
+        return left;
+    }
+}
---
+++ src/lesson02/Runner.java
@@ -0,0 +1,18 @@
+package lesson02;
+
+import java.util.Arrays;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        int[] array = new int[100];
+
+        for (int i = 0; i < array.length; ++i) {
+            array[i] = array.length - i;
+        }
+
+        System.out.println("Unsorted array:\n" + Arrays.toString(array));
+        QuickSort.sort(array);
+        System.out.println("Sorted array:\n" + Arrays.toString(array));
+    }
+}
